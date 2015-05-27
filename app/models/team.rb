a2c42c8e6276b9925496f1dba23c1bd5243e3b13
Jakub Wojtysiak <schizunio@gmail.com>
2015-05-27 19:36:23 +0200

    When there is a tie in score, show higher the team who answered first

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,7 +4,7 @@ class Team < ActiveRecord::Base
 
   class << self
     def scoresheet
-      all.where('staff IS FALSE').map{|team| { :team_id => team.id, :name => team.name, :score => team.score }}.sort{ |a,b| b[:score] <=> a[:score] }
+      all.where('staff IS FALSE').map{|team| { :team_id => team.id, :name => team.name, :score => team.score, :last_correct_guess => team.last_correct_guess.try(:created_at) || 0 }}.sort{ |a,b| [b[:score], a[:last_correct_guess]] <=> [a[:score], b[:last_correct_guess]] }
     end
   end
 
@@ -14,6 +14,10 @@ class Team < ActiveRecord::Base
 
   def guessed?(puzzle)
     !Guess.where(:puzzle_id => puzzle.id, :team_id => self.id, :correct => true).empty?
+  end
+
+  def last_correct_guess
+    Guess.where(:team_id => self.id, :correct => true).order("created_at DESC").first
   end
 
   def guess_count_for_puzzle(puzzle)
