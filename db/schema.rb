8d53e3873b48a1a5ddb8114b2b85041c674fb2bb
Marcin Bunsch <marcin.bunsch@gmail.com>
2015-05-27 19:59:38 +0200

    Add message support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527130211) do
+ActiveRecord::Schema.define(version: 20150527175500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,12 @@ ActiveRecord::Schema.define(version: 20150527130211) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "team_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "puzzles", force: :cascade do |t|
